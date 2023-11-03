036b31a197b4a22c6beccb5e52e66c92db93ca64
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2023-11-02 23:41:23 -0300

    update routes web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'autentication'])->name('login-autentication');
+
+Route::middleware(['front'])->group(function () {
+    // LoginController
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    // HomeController
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
 });

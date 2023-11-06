788fe024abdc38e8a9f2e6393831f0ffeb047fd8
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2023-11-05 21:05:53 -0300

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
